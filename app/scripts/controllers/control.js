e9578375ec7b9d7fb81084768623bc4b431b3bda
fgutekunst <fgutekunst@free.fr>
2015-03-31 13:29:44 +0200

    Ajout de champs texte dans Configuration

--- app/scripts/controllers/control.js
+++ app/scripts/controllers/control.js
@@ -8,6 +8,36 @@
  * Controller of the gpwebApp
  */
 angular.module('gpwebApp')
-  .controller('ControlCtrl', function ($scope) {
-  $scope.config = true;
-  });
+	.controller('ControlCtrl', ['$scope', 'pageData', function ($scope, pageData) {
+		 // $scope.config = true; //'views/config.html';
+		 $scope.config = {
+			  value: true,
+			  content:'Essai'
+			};
+		/* if(selectedItem.type === 'text')
+		 {
+			 $scope.config = {
+			  value: true,
+			  content:"Essai"
+			};
+			 
+		 } else if (selectedItem.type === 'image'){
+			 $scope.config = {
+			  value: true,
+			  content:"Essai"
+			};
+			 
+		 } else {
+			 $scope.config = {
+			  value: true,
+			  content:"Essai"
+			};
+		 }*/
+		  
+		 // $scope.config.content = { "<h4> Essai </h4>"};
+		  $scope.applyChanges = function()
+		  {
+			  
+			  
+		  }
+ }]);
