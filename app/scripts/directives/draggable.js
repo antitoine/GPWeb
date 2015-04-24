e1d6c8ec7d7d3d480d6938a7bb63dd38532cf340
Antoine Chabert <chabert.antoine@gmail.com>
2015-04-24 15:17:48 +0200

    Actualisation du modele avec le drag and drop et le resizable

--- app/scripts/directives/draggable.js
+++ app/scripts/directives/draggable.js
@@ -10,12 +10,21 @@ angular.module('gpwebApp')
   .directive('draggable', function () {
     return {
       restrict: 'A',
-      link: function postLink(scope, elem, attrs) {
+      link: function (scope, elem, attrs) {
         if (elem.data('draggable-containment')) {
           elem.draggable({ containment: elem.data('draggable-containment'), scroll: false });
         } else {
           elem.draggable();
         }
+        elem.on('dragstop', function (evt, ui) {
+          if (scope.zone.top) {
+            scope.zone.top = elem.position().top + 'px';
+          }
+          if (scope.zone.left) {
+            scope.zone.left = elem.position().left + 'px';
+          }
+          scope.$apply();
+        });
       }
     };
   });
--- app/scripts/directives/resizable.js
+++ app/scripts/directives/resizable.js
@@ -10,12 +10,21 @@ angular.module('gpwebApp')
   .directive('resizable', function () {
     return {
       restrict: 'A',
-      link: function postLink(scope, elem, attrs) {
+      link: function (scope, elem, attrs) {
         if (elem.data('resizable-containment')) {
           elem.resizable({ containment: elem.data('resizable-containment') });
         } else {
           elem.resizable();
         }
+        elem.on('resizestop', function (evt, ui) {
+          if (scope.zone.width) {
+            scope.zone.width = elem.width() + 'px';
+          }
+          if (scope.zone.height) {
+            scope.zone.height = elem.height() + 'px';
+          }
+          scope.$apply();
+        });
       }
     };
   });
